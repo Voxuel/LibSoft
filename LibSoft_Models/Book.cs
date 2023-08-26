333ad3458f46ac8a042f7315a5414761063a6178
Voxuel <Leo.Fridh@hotmail.com>
2023-08-26 13:04:03 +0200

    initial

---
+++ LibSoft_Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibSoft_Models;
+
+public class Book
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public string Title { get; set; }
+    
+    [Required]
+    public string Author { get; set; }
+    
+    public int Year { get; set; }
+    
+    [Required]
+    public string Genre { get; set; }
+    
+    public string Description { get; set; }
+}
