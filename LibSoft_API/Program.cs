3de77cdef2814482fd79df31093f9f7947814872
Voxuel <Leo.Fridh@hotmail.com>
2023-09-01 16:22:59 +0200

    add get all books

--- LibSoft_API/Program.cs
+++ LibSoft_API/Program.cs
@@ -1,11 +1,30 @@
+using System.Net;
+using AutoMapper;
+using FluentValidation;
 using LibSoft_API.Data;
+using LibSoft_API.MappingProfiles;
+using LibSoft_API.Services;
+using LibSoft_Models;
+using LibSoft_Models.API_Model_Tools;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LibSoftDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("connection")));
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddAutoMapper(typeof(BookMappingConfig));
+builder.Services.AddTransient<IBookRepository, BookRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/book",async (IBookRepository repo) =>
+{
+    APIResponse response = new APIResponse();
+    response.Result = await repo.GetAllBooks();
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.OK;
+    return Results.Ok(response);
+}).WithName("GetAllBooks").Produces<APIResponse>(200);
 
 app.Run();
--- LibSoft_Models/API_Model_Tools/APIResponse.cs
+++ LibSoft_Models/API_Model_Tools/APIResponse.cs
@@ -10,7 +10,7 @@ public class APIResponse
     }
 
     public bool IsSuccess { get; set; }
-    public object Result { get; set; }
+    public Object Result { get; set; }
     public HttpStatusCode StatusCode { get; set; }
     public List<string> ErrorMessages { get; set; }
 }
