6909284104f95b0c2c4dcfeb7060455a698cd15f
Voxuel <Leo.Fridh@hotmail.com>
2023-09-01 15:24:33 +0200

    add mapping config

---
+++ LibSoft_API/MappingProfiles/BookMappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LibSoft_Models;
+
+namespace LibSoft_API.MappingProfiles;
+
+public class BookMappingConfig : Profile
+{
+    public BookMappingConfig()
+    {
+        CreateMap<Book,BookDTO>().ReverseMap();
+        CreateMap<Book,BookCreateDTO>().ReverseMap();
+        CreateMap<Book, BookUpdateDTO>().ReverseMap();
+    }
+}
